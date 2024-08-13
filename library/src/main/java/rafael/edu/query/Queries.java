905c33bc975933b2c18bb8cc03f5070b0dde68c8
rafael taparica <rafael_taparica@outlook.com>
2024-08-12 22:29:43 -0300

    update list methods

--- library/src/main/java/rafael/edu/query/Queries.java
+++ library/src/main/java/rafael/edu/query/Queries.java
@@ -8,21 +8,23 @@ import javax.persistence.Persistence;
 
 import rafael.edu.entity.Book;
 import rafael.edu.entity.Customer;
+import rafael.edu.entity.RentRegistration;
+import rafael.edu.entity.Section;
 
 public class Queries {
 
 	private EntityManagerFactory emf;
 	private EntityManager em;
 
-	//BOOKS LISTS
+	// BOOKS LISTS
 	public void bookFullList() {
 		try {
 			Persistence.createEntityManagerFactory("library");
-			emf.createEntityManager();
+			em = emf.createEntityManager();
 			em.getTransaction().begin();
 			List<Book> bookList = em.createQuery("SELECT b FROM Book b", Book.class).getResultList();
 			em.getTransaction().commit();
-			for(Book b : bookList) {
+			for (Book b : bookList) {
 				System.out.println("Title: " + b.getTitle() + "\nAuthor: " + b.getAuthor());
 			}
 		} catch (Exception e) {
@@ -31,22 +33,69 @@ public class Queries {
 			em.close();
 			emf.close();
 		}
-
 	}
 	
-	//CUSTOMERS LISTS
+	/*
+	 * check query: select book list from one section.
+	 */
+
+	// CUSTOMERS LISTS
 	public void customerFullList() {
 		try {
 			Persistence.createEntityManagerFactory("library");
-			emf.createEntityManager();
+			em = emf.createEntityManager();
 			em.getTransaction().begin();
 			List<Customer> customerList = em.createQuery("SELECT c FROM Customer c", Customer.class).getResultList();
 			em.getTransaction().commit();
-			for(Customer c : customerList) {
+			for (Customer c : customerList) {
 				System.out.println("Name: " + c.getName() + "\nRegistration: " + c.getRegistration());
 			}
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.out.println(e.getMessage());
+		} finally {
+			em.close();
+			em.close();
+		}
+	}
+
+	// RENT REGISTRATIONs LISTS
+	public void rentRegistrationFullList() {
+		try {
+			Persistence.createEntityManagerFactory("library");
+			emf.createEntityManager();
+			em = emf.createEntityManager();
+			em.getTransaction().begin();
+			List<RentRegistration> rentRegistrationList = em
+					.createQuery("SELECT r FROM RentRegistration r", RentRegistration.class).getResultList();
+			em.getTransaction().commit();
+			for (RentRegistration r : rentRegistrationList) {
+				System.out.println("Customer: " + r.getCustomer() + "\nBook: " + r.getBook() + "\nRent date: "
+						+ r.getRentDate() + "\nReturn date: " + r.getReturnDate());
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		} finally {
+			em.close();
+			emf.close();
+		}
+	}
+
+	// SECTIONS LIST
+	public void sectionFullList() {
+		try {
+			Persistence.createEntityManagerFactory("library");
+			em = emf.createEntityManager();
+			em.getTransaction().begin();
+			List<Section> sectionList = em.createQuery("SELECT s FROM Section s", Section.class).getResultList();
+			em.getTransaction().commit();
+			for (Section s : sectionList) {
+				System.out.println("Name: " + s.getSectionName() + "\nRoW: " + s.getNumber());
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			em.close();
+			emf.close();
 		}
 	}
 }
